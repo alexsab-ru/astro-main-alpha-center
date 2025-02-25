b0cd46df0aa7e367eb2889cbebd9e27fb9cd1bbd
Oksana Turova <psdhtmlcss@mail.ru>
2025-02-25 14:57:18 +0400

    services collections

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,9 @@
 import { defineCollection } from 'astro:content';
 
 const seoCollection = defineCollection({});
+const servicesCollection = defineCollection({});
 
 export const collections = {
   seo: seoCollection,
+  services: servicesCollection
 };
